24b2c598cd6917aa0a0b3f17ff134525cd3d6463
Arek van Schaijk <info@ucreation.nl>
2015-08-27 21:23:02 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Site Essentials',
+	'description' => '',
+	'category' => 'plugin',
+	'version' => '1.0.0',
+	'state' => 'stable',
+	'uploadfolder' => FALSE,
+	'createDirs' => '',
+	'clearcacheonload' => FALSE,
+	'author' => 'Arek van Schaijk',
+	'author_email' => 'info@ucreation.nl',
+	'author_company' => 'Ucreation',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.0.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
